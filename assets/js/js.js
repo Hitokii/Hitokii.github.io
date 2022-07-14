8dedd523955da435915ce9a64c2568e9d30d9d40
Hitoki <phitoki@outlook.fr>
2022-07-14 18:21:27 +0200

    changed site
    Co-authored-by: Syhix <hello@syhix.tv>

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,12 +1,53 @@
 var i = 0;
+var y = 0;
+var ii = 0;
 var txt = '> Site crée par Hitoki et Syhix';
 var inputchar = '_';
-var speed = 30;
+var speed = 70;
+var logo =   [" *@@                                             @@  \n",
+              " *@@@@                                        *@@@@  \n",
+              " *@, @@@               @     @               @@  @@  \n",
+              " *@,   #@@              @   @             /@@    @@  \n",
+              " *@,@@@. /@@.   &@@@@@@@@@@@@@@@@@@@    @@@   @@.@@  \n",
+              " *@*@% (@@@@@@@@&                   @@@@@@@@@  @@@@  \n",
+              " *@@@                                          .@@@  \n",
+              " *@@                                            @@@  \n",
+              " *@@                                             @@  \n",
+              " *@@                                             @@  \n",
+              " *@@         @@                       @@         @@  \n",
+              " *@(          @@                    .@@          @@  \n",
+              " @@            @@                  @@@            @@ \n",
+              "@@/             @@                &@&             @@,\n",
+              "  @@@@.                                       (@@@@  \n",
+              "      @@@@(                               &@@@@      \n",
+              "          @@@@*                       @@@@@          \n",
+              "              @@@@@               @@@@@              \n",
+              "                  @@@@         @@@#                  \n",
+              "                     %@@@#@ @@@                      \n",
+              "                        *@@@                         \n"]
+
+
+
+function typeWriter2() {
+  if (ii < txt.length) {
+      document.getElementById("credit").innerHTML += txt.charAt(ii);
+      ii++;
+      setTimeout(typeWriter2, speed);
+    }
+  }
+
+typeWriter2()
+
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+  if (i < logo.length+1) {
+    while (y < 55) {
+      document.getElementById("demo").innerHTML += logo[i].charAt(y);
+      console.log(i + " " + y)
+      y++;
+    }
     i++;
+    y=0;
     setTimeout(typeWriter, speed);
   }
 }
